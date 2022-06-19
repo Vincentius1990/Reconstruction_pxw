67ad444956237a704d8ca515e70aa6eea45b43e1
Peng Xingwen <68174979+Vincentius1990@users.noreply.github.com>
2022-06-19 20:17:47 +0800

    Update mlp.py

--- mlp.py
+++ mlp.py
@@ -26,38 +26,34 @@ from src.loss.train_log import train_log_draw
 
 def train(dataset, model, net, optimizer, device, num_epochs, criterion):
     '''
-    训练网络，记录模型参数
+    train the neural networks
     '''
 
-    # 读取训练集和测试集
-    train_dataset = HeatsinkData(root, train_path, ind, None)                                             # 读取训练集
-    train_iter = DataLoader(train_dataset, batch_size = batch_size, shuffle= True, num_workers=16)      # 封装训练集
-    test_dataset = HeatsinkData(root, test_path, ind, None)                                               # 读取测试集
-    test_iter  = DataLoader(test_dataset, batch_size = batch_size, shuffle= False, num_workers=16)      # 封装测试集
-    l_train, l_test = train_dataset.__len__(), test_dataset.__len__()                                  # 获取训练集和测试集样本数量
-    train_log = np.zeros([num_epochs, 6])                                                              # 保存训练过程2个loss和4个指标
+    train_dataset = HeatsinkData(root, train_path, ind, None)                                            
+    train_iter = DataLoader(train_dataset, batch_size = batch_size, shuffle= True, num_workers=16)     
+    test_dataset = HeatsinkData(root, test_path, ind, None)                                               
+    test_iter  = DataLoader(test_dataset, batch_size = batch_size, shuffle= False, num_workers=16)    
+    l_train, l_test = train_dataset.__len__(), test_dataset.__len__()                                 
+    train_log = np.zeros([num_epochs, 6])                                                            
     
-    print('Training on', device, 'model = ' + str(model), 'loss = MAE')             # 模型和loss信息
-    print('dataset = ' + str(dataset), 'train_set samples:', l_train, ', test_samples:', l_test)       # 数据集信息                                         
+    print('Training on', device, 'model = ' + str(model), 'loss = MAE')             
+    print('dataset = ' + str(dataset), 'train_set samples:', l_train, ', test_samples:', l_test)                                        
 
     for epoch in range(num_epochs):
-        ## 训练集上训练
-        train_loss, test_loss, start = 0.0, 0.0, time.time()        # 训练误差 (MAELoss), 测试误差, 训练开始时间赋初值
+        train_loss, test_loss, start = 0.0, 0.0, time.time()        
         
         net.train()
         for _, data1 in enumerate(train_iter):
             X1, y1 = data1
-            X1, y1 = X1.to(device), y1.to(device)                   # 数据转存到GPU上
-            output1 = net(X1)                                       # 读取数据进行预测
-            # print('X1:', X1.shape, 'y1:', y1.shape, 'output1:', output1.shape)             
-            l1 = criterion(y1, output1)                            # L1 loss计算       
-            optimizer.zero_grad()                                   # 梯度清零
-            l1.backward()                                           # 反向传播
-            optimizer.step()                                        # 参数优化
-            train_loss += l1.cpu().item() * y1.size(0)              # train_loss累加
+            X1, y1 = X1.to(device), y1.to(device)                  
+            output1 = net(X1)                                                   
+            l1 = criterion(y1, output1)                              
+            optimizer.zero_grad()                                  
+            l1.backward()                                          
+            optimizer.step()                                        
+            train_loss += l1.cpu().item() * y1.size(0)             
         scheduler.step()
 
-        ## 测试集上测试
         net.eval()
         with torch.no_grad():
             for _, data2 in enumerate(test_iter):
@@ -65,7 +61,7 @@ def train(dataset, model, net, optimizer, device, num_epochs, criterion):
                 X2 = X2.to(device)
                 y2 = y2.to(device)                  
                 output2 = net(X2)                
-                l2 = criterion(y2, output2)                        # L1 loss 计算 
+                l2 = criterion(y2, output2)                       
                 test_loss += l2.cpu().item() * y2.size(0)
 
         train_log[epoch][0], train_log[epoch][1] = train_loss / l_train, test_loss / l_test  
@@ -73,17 +69,14 @@ def train(dataset, model, net, optimizer, device, num_epochs, criterion):
         train_log[epoch][4], train_log[epoch][5] = 0, 0    
         print('epoch {}, Train loss {:.6f}, Test loss: {:.6f}, time {:.1f}s'.format(epoch + 1, train_loss / l_train, test_loss / l_test, time.time()-start))
     
-    torch.save(net, path1)                                                                           # 保存网络   
-    torch.save(net.state_dict(), path2)                                                              # 保存模型参数  
+    torch.save(net, path1)                                                                           
+    torch.save(net.state_dict(), path2)                                                               
     np.save('results/runs/' + str(dataset[0:2]) + '/MLP/'+ str(dataset) + '_' + str(model)+ '_' \
-    + str(num_epochs) + 'epoch/train_log', train_log)                                                # 保存指标
+    + str(num_epochs) + 'epoch/train_log', train_log)                                               
     print('dataset = ' + str(dataset) + ', train_set samples:', l_train, ', test_samples:', l_test)
 
 
 if __name__ == '__main__':
-    '''
-    主函数,指定路径,定义超参数,网络等
-    '''
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     dataset = 'vp_10_16ob'
@@ -99,16 +92,13 @@ if __name__ == '__main__':
     net = MLPV2(num_inputs, num_hiddens1, num_hiddens2, num_outputs)                                
     net = net.to(device)
                         
-    criterion = torch.nn.L1Loss()        # 训练loss, L1 loss, 乙可采用torch.nn.MSELoss(),效果相当
-    lr = 0.01                            # 学习率
-    num_epochs = 100                     # 训练迭代次数
-    st = time.time()                     # 训练及测试总时间
+    criterion = torch.nn.L1Loss()       
+    lr = 0.01                            
+    num_epochs = 100                     
+    st = time.time()                    
     
-    optimizer = torch.optim.Adam(net.parameters(), lr=lr)            # 采用Adam优化算法
-    scheduler = ExponentialLR(optimizer, gamma=0.97)                 # 指数变化学习率
-    # optimizer = torch.optim.Adam(net.parameters(), lr=lr, betas=(0.9, 0.999), eps=1e-08, weight_decay=0)         #采用Adam优化算法        
-    # scheduler = MultiStepLR(optimizer, milestones=[10, 20, 30, 40, 50, 60, 80, 100, 120, 150, 180], gamma=0.5)   #里程碑式变学习率
-    # optimizer = torch.optim.SGD(net.parameters(), lr=lr, weight_decay=5e-4, momentum=0.9)                        # SGD优化算法
+    optimizer = torch.optim.Adam(net.parameters(), lr=lr)            
+    scheduler = ExponentialLR(optimizer, gamma=0.97)                                    
 
     dirs = '/mnt/share1/pengxingwen/reconstruction_pxw/results/runs/' + str(dataset[0:2]) + '/MLP/'+ str(dataset) + '_' + str(model)+ '_' + str(num_epochs) + 'epoch'
     if not os.path.exists(dirs):
