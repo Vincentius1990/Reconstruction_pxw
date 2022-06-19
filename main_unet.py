5abd8fc99461868ff7807a531f2a234b27aef7a1
Peng Xingwen <68174979+Vincentius1990@users.noreply.github.com>
2022-06-19 20:10:07 +0800

    Update main_unet.py

--- main_unet.py
+++ main_unet.py
@@ -1,3 +1,17 @@
+# encoding: utf-8
+#    Authors: Xingwen Peng
+#    National University of Defense Technology, China
+#    Defense Innovation Institute, Chinese Academy of Military Science, China
+#    EMAIL: vincent1990@126.com
+#    DATE:  Jun 2022
+# ------------------------------------------------------------------------
+# This code is part of the program that produces the results in the following paper:
+#
+# Peng, X., Li, X., Gong, Z., Zhao, X., Yao, W., 2022. A Deep Learning Method Based on Partition Modeling For reconstructing Temperature Field. SSRN Journal. https://doi.org/10.2139/ssrn.4065493
+#
+# You are free to use it for non-commercial purposes. However, we do not offer any forms of guanrantee or warranty associated with the code. We would appreciate your acknowledgement.
+# ------------------------------------------------------------------------
+
 import os
 import time
 import numpy as np
